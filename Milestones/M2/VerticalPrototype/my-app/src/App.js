5326b76d519c18b4b90649179deb52a044917e74
Johny <jvaladez92000@gmail.com>
2023-04-04 12:59:36 -0700

    db movement test

--- Milestones/M2/VerticalPrototype/my-app/src/App.js
+++ Milestones/M2/VerticalPrototype/my-app/src/App.js
@@ -11,6 +11,26 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import MainContent from './components/Maincontent';
 
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: 'database-1.cjhdgriivebl.us-west-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password1',
+  database: 't6db'
+})
+
+
+db.connect((error) => {
+  if(error){
+    console.log('Error connecting to the MySQL Database');
+    return;
+  }
+  console.log('Connection established sucessfully');
+});
+connection.end((error) => {
+});
+
 
 function App() {
 
