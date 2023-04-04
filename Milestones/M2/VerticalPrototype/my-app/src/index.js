67edf0e81f305c3cc16acd343cab18390aaae26b
Johny <jvaladez92000@gmail.com>
2023-04-03 22:52:29 -0700

    db test redirecting.

--- Milestones/M2/VerticalPrototype/my-app/src/index.js
+++ Milestones/M2/VerticalPrototype/my-app/src/index.js
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const mysql = require("mysql");
-
-const db = mysql.createPool({
-  host: 'database-1.cjhdgriivebl.us-west-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'password1',
-  database: 't6db'
-})
-
-const sqlInsert = "INSERT INTO Tutors (TutorID,TutorFIrstName, TutorLastName, TutorPhone, TutorEmail) VALUES ('4', 'John','Doe', '559-123-4567','john.doe@gmail.com');"
-db.query(sqlInsert, (err,result)=> {
-  console.log('inserted to db');
-})
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
