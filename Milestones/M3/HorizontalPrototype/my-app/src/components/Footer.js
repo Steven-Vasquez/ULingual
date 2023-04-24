bd4c03ea928bd873f08936ac495a7007f5308e0c
Johny <jvaladez92000@gmail.com>
2023-04-23 21:56:39 -0700

    error fixing

--- Milestones/M3/HorizontalPrototype/my-app/src/components/Footer.js
+++ Milestones/M3/HorizontalPrototype/my-app/src/components/Footer.js
@@ -4,9 +4,14 @@ import './Footer.css'
 function Footer() {
     return(
     <div className='footing'>
-        <Link to="ContactUs">Contact Us</Link>
-        <Link to="AboutUs">About Us</Link>
-        <Link to="FAQ">FAQ</Link>
+        <ul className="nav-item">
+        <li><Link to="ContactUs">Contact Us</Link>
+        </li>
+        <li><Link to="AboutUs">About Us</Link>
+        </li>
+        <li><Link to="FAQ">FAQ</Link>
+        </li>
+        </ul> 
     </div>
         /* FOOTER
         * - ULingual actions:
