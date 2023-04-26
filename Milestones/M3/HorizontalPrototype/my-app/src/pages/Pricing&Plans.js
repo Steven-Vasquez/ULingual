b7e6ae84c99a0726623719427a397d2d6627340d
Johny <jvaladez92000@gmail.com>
2023-04-26 11:45:37 -0700

    Pricing&Plans page Style Update

--- Milestones/M3/HorizontalPrototype/my-app/src/pages/Pricing&Plans.js
+++ Milestones/M3/HorizontalPrototype/my-app/src/pages/Pricing&Plans.js
@@ -1,12 +1,31 @@
 import React from "react";
-import './StyleSheets/ComingSoon.css';
+import './StyleSheets/PricingPage.css';
 
  const Pricing = () => {
     return (
-        <div className="TutoringPlans">
-        <h1>TutoringPlans</h1>
-        <p>Basic features-tbd-are free.</p>
-        <p>Monthly $5 subscription to access more advanced, personalized features.</p>
+        <div className="Plans">
+        <h1>Plans We Offer</h1>
+        <p>Here at ULingual we believe in transparency,
+            this is why we offer two weeks free of our content to
+            allow you to see if we are a fit for you.
+        </p>
+        <p>Like what we have? Choose one of our plans and 
+            welcome to our community!
+        </p>
+        <ul className="plan-options">
+            <li>
+                $7 Monthly Subscription 
+                <br></br>
+                <p2>Have access to all our content!</p2>
+            </li>
+
+            <li>
+                $65 Yearly Subscription
+                <br></br>
+                <p2>Have access to all our content plus 
+                    3 free tutor sessions!</p2>
+            </li>
+        </ul>
         </div>
      );
 
