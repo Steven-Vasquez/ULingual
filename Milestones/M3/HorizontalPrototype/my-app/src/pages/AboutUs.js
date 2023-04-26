61f7302f779a00f8b5baf77911cb0a0dfc3e34c2
Johny <jvaladez92000@gmail.com>
2023-04-26 10:35:21 -0700

    AboutUs page style fixing

--- Milestones/M3/HorizontalPrototype/my-app/src/pages/AboutUs.js
+++ Milestones/M3/HorizontalPrototype/my-app/src/pages/AboutUs.js
@@ -1,19 +1,37 @@
 import React from "react";
-import '../index.css';
+import './StyleSheets/AboutUs.css';
 
 
 const AboutUs = () => {
     return (
         <div className="AboutUs">
-      <h1>About Us: Our mission</h1>
-        <p>ULingual is a Language exchange platform that connects native speaker with non-native speakers of a given language Users can learn and practise speaking the language with each other over a video streaming connection to experience a more personal and intriguing learning environment.</p>
-        <p>ULingual understands that learning language is difficult;practicing by speaking is great. It is also important to hear the language often.Ulingual recommends movies, television shows, and books based on the languague you want to learn so that way you can further envelop yourself and get a better grasp of the language.
+      <h1>About Us: Our Mission</h1>
+        <p>ULingual is a language exchange platform that connects native speaker with non-native speakers of a given language.
+           Users at ULingual can learn and practice speaking a language with each other over a video streaming connection 
+           to experience a more personal and intriguing learning experience.</p>
+        <br></br>
+        <p>ULingual understands that learning language can be difficult.
+          Practicing by speaking is an important component of learning. 
+          At ULingual we also believe in practice by hearing, getting adjusted to a language by hearing it is a great way of learning.
+          This is why at ULingual we offer curated music playlists, movies & show recommendations, and boook recommendations
+          to further create a more unique learning experience.
          </p>
-         <p>what makes ULingual stands out from the rest, lies in our alternative learnng methods. Speaking the language is half half the battle</p>
-         <p>Here at ULingual we believe it is critical to immerse yourself in the language by getting involved with its culture, whether through music, books, movies, or television.</p>
-         <p>Come and join us in a judgment free enviornment, collaborating and improving each other's knowledge and skill.</p>
-         <p>ULingual offers affordable plan that can suit to everyone. It also offer two weeks free of permium content</p>
-        </div>
+         <br></br>
+         <p>What makes ULingual stands out from the rest lies in our alternative learnng methods.
+           Speaking the language is only half the battle, learning by hearing and recognizing phrases
+           and sayings from the language is the other half.</p>
+         <br></br>
+         <p>Here at ULingual we believe it is critical to immerse yourself in the language by getting
+           involved with its culture, whether through music, books, movies, or television.</p>
+         <br></br>
+         <p>Come and join us in a judgment free enviornment,
+           collaborating and improving each other's knowledge and skill within the 
+           comfort of your home.</p>
+         <br></br>
+         <p>ULingual offers affordable plans that can suit to everyone.
+           Sign up and get your first two weeks for free to see the great community
+           and courses that ULingual offers.</p>
+        </div> 
      );
 
 }
