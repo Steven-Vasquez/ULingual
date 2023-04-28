652e2b16c275d9b29a5a7601c7d8f9d9647c5f5e
Niqueshahrulnafiz <niqueshahrulnafiz@gmail.com>
2023-04-27 18:10:45 -0700

    Update - closed link on User profile

--- Milestones/M3/HorizontalPrototype/my-app/src/pages/UserProfile.js
+++ Milestones/M3/HorizontalPrototype/my-app/src/pages/UserProfile.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 const UserProfile = () => {
     return(
     <div className="user-entire1">
-            <div className='UserProfile1'>
-                <div className="User-Info1">
-                    <img className="ProfileImage1" src ={"https://placehold.jp/150x150.png"}/>
-                    <h2 className="ProfileName1">User Name</h2>
-                </div>
-                <div className="UserDetails1">
+        <div className='UserProfile1'>
+            <div className="User-Info1">
+                <img className="ProfileImage1" src ={"https://placehold.jp/150x150.png"}/>
+                <h2 className="ProfileName1">User Name</h2>
+            </div>
+            <div className="UserDetails1">
                 <h3>
-                    <ul>
-                         <div className="desc1" >
+                    <
+                        <div className="desc1" >
                             <li>Description</li>
                         </div>
                         <div className="friends1">
@@ -21,15 +21,12 @@ const UserProfile = () => {
                         </div>
                         <div className="lang-sel1">
                             <li>Language Selected</li>
-                            </Link>
+                            <Link to="/UserProfileEdit">Edit Profile</Link>
                         </div>
                     </ul>
                 </h3>
-                 </div>
-                 <label>
-                    <Link to="/UserProfileEdit">Edit Profile</Link>
-                </label>  
-            </div>   
+            </div>
+        </div>   
     </div>
     );
 }
