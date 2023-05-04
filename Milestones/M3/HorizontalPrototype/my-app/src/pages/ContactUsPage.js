e4c075e14801999cc60461641cb5fcfec99cf525
AidanStivers <aidan.stivers@gmail.com>
2023-05-03 22:43:33 -0700

    ContactUs Validation (front-end only)

--- Milestones/M3/HorizontalPrototype/my-app/src/pages/ContactUsPage.js
+++ Milestones/M3/HorizontalPrototype/my-app/src/pages/ContactUsPage.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import './stylesheets/ContactUsPage.css';
 
 const ContactPage = () => {
+
+    const [errorMessage, setErrorMessage] = useState({
+        username: '',
+        email: '',
+    });
+
+    const validateUsername = (username) => {
+        let error = '';
+        if (username.trim() === '') {
+          error = 'Username required';
+        }
+        setErrorMessage((prevState) => ({ ...prevState, username: error }));
+      };
+
+    const validateEmail = (email) => {
+        let error = '';
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (email.trim() === '') {
+          error = 'Email is required';
+        }
+        else if (!emailRegex.test(email) ){
+            error = 'Invalid email format';
+        }
+        setErrorMessage((prevState) => ({ ...prevState, email: error }));
+      };  
+
        return (
     <div className="contact-page">
         <div className="contact">
             <form className="form">
                 <h1>Contact Us</h1>
                 <div>
-                    <input type="text" placeholder="Username"/>
+                    <input type="text" placeholder="Username" required
+                    onBlur={(e) => validateUsername(e.target.value)}
+                    />
+                    {errorMessage.username && (<span className="error">{errorMessage.username}</span>)}
                 </div>
                 <div>
-                <input type="email" placeholder="Email"/>
+                <input type="email" placeholder="Email" required
+                onBlur={(e) => validateEmail(e.target.value)}
+                />
+                {errorMessage.email && (<span className="error">{errorMessage.email}</span>)}
                 </div>
                 <div>
                 <textarea name="Message" cols={30} rows={10} placeholder="Write your message here..."/>
