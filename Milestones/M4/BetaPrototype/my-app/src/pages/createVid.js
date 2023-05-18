23356962cb5254ad7b5ee1b0db417f44e485e37f
Johny <jvaladez92000@gmail.com>
2023-05-18 06:15:22 -0700

    create video room update: needs backend to gen ID!

--- Milestones/M4/BetaPrototype/my-app/src/pages/createVid.js
+++ Milestones/M4/BetaPrototype/my-app/src/pages/createVid.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+import './stylesheets/CreateVideo.css'
+
 const CreateVid = ()  => {
     return(
-        <div>
-            <h2>testing</h2>
+        <div className="create-pg">
+            <h2>Create a Video Room ID</h2>
+                <div className="create-id-pg">
+                    <form className="create-id-form">
+                    
+                        <button type="submit" className="button">
+                             Generate ID!
+                        </button>
+
+                    </form>
+
+
+                </div>
         </div>
     );
 }
