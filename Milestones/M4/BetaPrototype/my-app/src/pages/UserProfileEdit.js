05e61101da93a66411af780ac1e5adaf23485728
Niqueshahrulnafiz <niqueshahrulnafiz@gmail.com>
2023-05-17 23:56:31 -0700

    Partial fix on edit page

--- Milestones/M4/BetaPrototype/my-app/src/pages/UserProfileEdit.js
+++ Milestones/M4/BetaPrototype/my-app/src/pages/UserProfileEdit.js
@@ -7,26 +7,40 @@ const UserProfileEdit = () => {
     <div className="user-entire">
             <div className='UserProfile'>
                 <div className="User-Info">
+                    <h4>Edit Photo</h4>
                     <img className="ProfileImage" src ={"https://placehold.jp/150x150.png"}/>
                 </div>
                         
                 <div className="descriptionContainer">
-                    <input type="text" placeholder="Enter New Description"/>
+                    <input type="text" placeholder="Enter New Description" className="editDescription"/>
                 </div>
                             
                 <div className="user-status">
-                    <h4>Status</h4> 
+                    <h4>Change Status</h4> 
                     <div className="online">
-                        <li>Online</li>
+                        <h3>Online</h3>
                     </div>
                     <div className="offline">
-                        <li>Offline</li>
+                        <h3>Offline</h3>
                     </div>
                     <div className="dnd">
-                        <li>Do No Disturb</li>
+                        <h3>Do No Disturb</h3>
                     </div>
                 </div>
-                
+
+                <div className="langSelectionBox">
+                <h2>Select Languages</h2>
+                <div className="innerLangSelection">
+                    <ul>
+                        <li>English <input type={"checkbox"}/></li>
+                        <li>Korean <input type={"checkbox"}/></li>
+                        <li>Spanish <input type={"checkbox"}/></li>
+                        <li>French <input type={"checkbox"}/></li>
+                        <li>Arabic <input type={"checkbox"}/></li>
+                    </ul>
+                </div>
+                </div>
+
                 <div className="button">
                     <Link to="/UserProfile" className="greenbox">Done</Link>
                 </div>
