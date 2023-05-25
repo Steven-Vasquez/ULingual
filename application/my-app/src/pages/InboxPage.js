e4713c08c61056242a7b1fe3115b6d7a447cb663
Johny <jvaladez92000@gmail.com>
2023-05-25 00:04:52 -0700

    inbox redo-for information display only.

--- application/my-app/src/pages/InboxPage.js
+++ application/my-app/src/pages/InboxPage.js
@@ -77,38 +77,10 @@ const InboxPage = () => {
             <div className="inboxHeader">
                 <h1>Inbox</h1>
             </div>
-
             <div className="innerInbox">
-                <div className="leftInbox">
-                    <div className="button">
-                        <Link to="/InboxPage" className="purplebox">Message</Link>
-                    </div>
-                    <div className="friendsInbox">
-                        <h3>Friend 1</h3>
-                        <h3>Friend 2</h3>
-                        <h3>Friend 3</h3>
-                        <h3>Friend 4</h3>
-                        <h3>Friend 5</h3>
-                        <h3>Friend 6</h3>
-                        <h3>Friend 7</h3>
-                        <h3>Friend 8</h3>
-                        <h3>Friend 9</h3>
-                        <h3>Friend 10</h3>
-                        <h3>Friend 11</h3>
-                        <h3>Friend 12</h3>
-                        <h3>Friend 13</h3>
-                        <h3>Friend 14</h3>
-                    </div>
-                </div>
-
-                <div className="rightInbox">
-                    <div className="userDetailInbox">
-                    
-                    </div>
-                    <div className="contentInbox">
                         {messages.map(message => (
                             <ul key={message.MessageID}>
-                                <div>Title: {message.Title} Date Sent:{new Date(message.DateSent).toLocaleTimeString()}, {new Date(message.DateSent).toLocaleDateString()}</div>
+                                <div className="inbox-request">Title: {message.Title} Date Sent:{new Date(message.DateSent).toLocaleTimeString()}, {new Date(message.DateSent).toLocaleDateString()}</div>
                                 <div>
                                     {message.Content}
                                 </div>
@@ -116,12 +88,6 @@ const InboxPage = () => {
                                 <button onClick={(e) => handleReject(e, message.MessageID)}>No</button>
                             </ul>
                         ))}
-                    </div>
-                    <div className="typeMessage">
-                        <input type="text" className="inboxInputText" placeholder="Something to say?!..." style={{ fontSize: '20px' }} />
-                        <button className="submitButtonText">Post</button>
-                    </div>
-                </div>
             </div>
         </div>
         </div>
